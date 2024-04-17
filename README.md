6e5338b3fb26151b9850dbe865e989a01e0b6088
Ondrej Babec <ond.babec@gmail.com>
2024-04-17 21:39:52 +0200

    Add COPYRIGHT

---
+++ README.md
@@ -0,0 +1,13 @@
+# Distribution Builder API
+Project is built on top of Spring Boot framework.
+It serves and back-end API for Angular UI located in the [repository](https://github.com/obabec/dist-builder-angular-ui)
+
+## Endpoints
+* Generate distribution  -- `/generateDistribution`
+* Get metadata -- `/metadata`
+
+More information about the endpoint can be found in JavaDoc.
+
+
+## Run the API
+`./mvnw spring-boot:run`
--- src/main/java/io/debezium/dist/builder/ui/controller/ServerController.java
+++ src/main/java/io/debezium/dist/builder/ui/controller/ServerController.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) [2024] [Ondrej Babec <ond.babec@gmail.com>]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package io.debezium.dist.builder.ui.controller;
 
 import java.io.ByteArrayOutputStream;
@@ -27,6 +50,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import static java.util.Objects.nonNull;
 
+/**
+ *
+ */
 @Controller
 @CrossOrigin(origins = "*")
 public class ServerController {
@@ -35,6 +61,13 @@ public class ServerController {
     @Value("${debezium.server.repository.root.path}")
     private String repositoryPath;
 
+    /**
+     * Generate distribution endpoint.
+     * @param distribution Serialized distribution string.
+     * @param truststore Truststore located as part of the mulitpart file.
+     * @param keystore Keystore located as part of the mulitpart file.
+     * @return Zip with complete distribution.
+     */
     @RequestMapping(
             value = "/generateDistribution",
             method = RequestMethod.POST,
@@ -62,6 +95,11 @@ public class ServerController {
         return new ResponseEntity<>(resource, HttpStatus.OK);
     }
 
+    /**
+     *
+     * @return
+     * @throws IOException
+     */
     @RequestMapping(
             value = "/metadata",
             produces="application/json",
